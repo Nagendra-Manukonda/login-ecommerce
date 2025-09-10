234989a87f05282541a7c3bd1125b1452d26a59c
Nagendra Manukonda <nagendra.manukonda@jsspro.com>
2025-09-10 14:49:23 +0530

    layout are updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <MainHeader />
+        <MainHeader onSearch={() => {}} />
         <main className="pt-12 max-h-full">{children}</main>
       
       </body>
